b2e49410eaa758b020ad8e8441a5e65d4272f70d
Brian Camacho <bcamacho@anduril.com>
2024-03-21 23:46:29 -0700

    rust impl

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,68 +1,142 @@
 use std::collections::HashMap;
-struct Args {
-    inventory_path: std::path::PathBuf
+pub struct Args {
+    pub inventory_path: std::path::PathBuf,
+    pub cart_path: std::path::PathBuf,
 }
 
-struct Listing {
+#[derive(Clone, Debug)]
+pub struct Listing {
     name: String,
-    price: i32
+    price: i32,
+    price_disc: Option<i32>,
 }
 
-type Barcode = i32;
+pub type Barcode = i32;
 
-type Inventory = HashMap<Barcode, Listing>;
+pub type Inventory = HashMap<Barcode, Listing>;
 
-struct ReceiptLine<'a> {
+#[derive(Clone, Debug)]
+pub struct ReceiptLine<'a> {
     quantity: i32,
-    what: &'a Listing
+    what: &'a Listing,
 }
 
-type Receipt<'a> = HashMap<Barcode, ReceiptLine<'a>>;
-
-impl Receipt {
-    pub fn new() {
-        HashMap::new()
+#[derive(Clone, Debug)]
+pub struct Receipt<'a> {
+    pub items: HashMap<Barcode, ReceiptLine<'a>>,
+    pub disc: bool,
+}
+impl<'a> Receipt<'a> {
+    pub fn new() -> Self {
+        Receipt {
+            items: HashMap::new(),
+            disc: false,
+        }
     }
 }
-impl Display for Receipt {
-    pub fn display(/*...*/) {
-        //todo: formatting here
 
+pub fn show_receipt(receipt: &Receipt) {
+    let is_disc = receipt.disc;
+    let totals: Vec<(_, _, _, _)> = receipt
+        .items
+        .iter()
+        .map(|(_, line)| {
+            let price = if is_disc {
+                line.what.price_disc.unwrap_or(line.what.price)
+            } else {
+                line.what.price
+            };
+            let subtotal = (line.quantity * price) as f64 / 100f64;
+            let savings = (line.quantity * line.what.price) as f64 / 100f64 - subtotal;
+            (line.quantity, line.what.name.clone(), subtotal, savings)
+        })
+        .collect();
 
+    let mut actual_total = 0f64;
+    let mut total_savings = 0f64;
+    for total in &totals {
+        actual_total += total.2;
+        total_savings += total.3;
     }
 
+    let lines: Vec<String> = totals
+        .iter()
+        .map(|(quantity, name, subtotal, _)| format!("{name} ({quantity}): ${subtotal:.2}"))
+        .collect();
+    println!("{}", lines.join("\n"));
+    println!("Total: ${actual_total:.2}, Savings: ${total_savings:.2}")
 }
 
-fn scan_item<'a>(barcode: Barcode, receipt: &mut Receipt<'a>,  inventory: &'a Inventory) -> Result<(), String> {
-    if let Some(r_line) = receipt.get_mut(&barcode) {
+pub fn scan_item<'a>(
+    barcode: Barcode,
+    receipt: &mut Receipt<'a>,
+    inventory: &'a Inventory,
+) -> Result<(), String> {
+    if let Some(r_line) = receipt.items.get_mut(&barcode) {
         r_line.quantity += 1;
-        return Ok(())
+        return Ok(());
     }
 
     if let Some(what) = inventory.get(&barcode) {
-        receipt.insert(barcode.clone(), ReceiptLine{ quantity: 1, what} );
+        receipt
+            .items
+            .insert(barcode.clone(), ReceiptLine { quantity: 1, what });
         Ok(())
     } else {
         Err(format!("no item with barcode: {}", barcode))
     }
 }
 
-fn parse_inventory(text: &str) -> Option<Inventory> {
-    unimplemented!("serde json parsing here")
+pub fn parse_inventory(text: &str) -> Option<Inventory> {
+    let mut inv = Inventory::new();
+    for line in text.trim().lines() {
+        let fields: Vec<&str> = line.split(" ").collect();
+        match fields[..] {
+            ["I", barcode, name] => {
+                inv.insert(
+                    barcode.parse::<i32>().ok()?,
+                    Listing {
+                        name: name.to_owned(),
+                        price: 0,
+                        price_disc: None,
+                    },
+                );
+            }
+            ["P", barcode, price] => {
+                inv.get_mut(&barcode.parse::<i32>().ok()?)?.price = price.parse::<i32>().ok()?
+            }
+            ["D", barcode, price_disc] => {
+                inv.get_mut(&barcode.parse::<i32>().ok()?)?.price_disc =
+                    Some(price_disc.parse::<i32>().ok()?);
+            }
+            _ => return None,
+        };
+    }
+    Some(inv)
 }
 
-fn parse_barcodes(text: &str) -> Option<Vec<i32>> {
-    unimplemented!("serde json parsing barcodes")
+pub fn parse_barcodes(text: &str) -> Option<(Vec<i32>, bool)> {
+    let mut barcodes = Vec::new();
+    let mut is_discount = false;
+    for line in text.trim().lines() {
+        let fields: Vec<&str> = line.split(" ").collect();
+        match fields[..] {
+            ["I", barcode] => barcodes.push(barcode.parse::<i32>().ok()?),
+            ["D", _] => is_discount = true,
+            _ => return None,
+        };
+    }
 
+    Some((barcodes, is_discount))
 }
 
-fn parse_args(argv: &Vec<&str>) -> Option<Args> {
-    if argv.len() != 3 || argv[1] != "--inventory" {
-        //todo: import log facade
-        //log::error!("");
+pub fn parse_args(argv: &Vec<String>) -> Option<Args> {
+    if argv.len() != 3 {
         None
     } else {
-        Some ( Args { inventory_path: argv[2].to_owned().into() } )
+        Some(Args {
+            inventory_path: argv[1].to_owned().into(),
+            cart_path: argv[2].to_owned().into(),
+        })
     }
 }
-
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,29 +1,25 @@
+use rcpt::{
+    parse_args, parse_barcodes, parse_inventory, scan_item, show_receipt, Args, Barcode, Inventory,
+    Listing, Receipt, ReceiptLine,
+};
 use std::io::Read;
 
-mod lib;
-
 fn main() {
+    let usage = "Usage: receipt /path/to/inventory /path/to/cart";
     let os_args = std::env::args().collect::<Vec<String>>();
-    
-    let args = parse_args(&os_args)
-        .expect("usage: echo <barcodes json> | receipt --inventory /path/to/inventory.json");
 
-    let inventory_text = std::io::read_to_string(args.inventory_path.read()).expect("inventory: no file");
+    let args = parse_args(&os_args).expect(usage);
+
+    let inventory_text = std::fs::read_to_string(args.inventory_path).expect("inventory: no file");
     let inventory = parse_inventory(&inventory_text).expect("inventory: no parse");
 
-    let mut barcode_text = Vec::new();
-    std::io::stdin().read_to_end(&mut barcode_text);
-    let barcodes = parse_barcodes(barcode_text);
+    let mut barcode_text = std::fs::read_to_string(args.cart_path).expect("cart: no file");
+    let (barcodes, is_discount) = parse_barcodes(&barcode_text).expect("cart: no parse");
 
     let mut receipt = Receipt::new();
+    receipt.disc = is_discount;
     for barcode in barcodes {
-        scan_item(barcode, &mut receipt, &inventory)
+        scan_item(barcode, &mut receipt, &inventory).expect("bad scan");
     }
-
-    println!("{receipt}");
-
-
-    println!("Hello, world!");
-    
+    show_receipt(&receipt);
 }
-
