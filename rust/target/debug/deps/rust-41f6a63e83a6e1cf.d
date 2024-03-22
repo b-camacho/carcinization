7a39c5ae0faa43b2238ae966ff20874fc63648f6
Brian Camacho <bcamacho@anduril.com>
2024-03-21 23:45:50 -0700

    no target though

--- rust/target/debug/deps/rust-41f6a63e83a6e1cf.d
+++
@@ -1,6 +0,0 @@
-/Users/bcamacho/sources/grocer/rust/target/debug/deps/librust-41f6a63e83a6e1cf.rmeta: src/main.rs src/lib.rs
-
-/Users/bcamacho/sources/grocer/rust/target/debug/deps/rust-41f6a63e83a6e1cf.d: src/main.rs src/lib.rs
-
-src/main.rs:
-src/lib.rs:
